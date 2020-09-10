1207877641e568fba944e8332bdf1032247664df
Tomati <dopedup@mail.ru>
2020-09-10 16:21:52 +0200

    Added request to edit a type object

--- Frontend/inventaire-minet/src/app/objects-position-add-section/objects-position-add-section.component.ts
+++ Frontend/inventaire-minet/src/app/objects-position-add-section/objects-position-add-section.component.ts
@@ -12,12 +12,12 @@ export class ObjectsPositionAddSectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
+    //const API_URL_EDIT_DEMAND = "http://localhost:8080/editDemand";
 
     parameters = [];
     addParameter(newParameter : string) : void {
       if(newParameter) {
-        this.parameters.push(newParameter);
+        this.parameters.push(newParameter.toUpperCase());
       }
     }
 
@@ -36,6 +36,21 @@ export class ObjectsPositionAddSectionComponent implements OnInit {
       let parameterIndex  = this.searchForParameter(parameter);
       this.parameters.splice(parameterIndex,1);
       //alert(parameter);
+    }
+
+    TryToEdit(objectType : string) {
+      let uppercase_objectType = objectType.toUpperCase();
+      const editDemand = { uppercase_objectType };
+      const API_URL_EDIT_DEMAND = "http://localhost:8080/editDemand";
+      console.log(editDemand);
+
+      fetch(API_URL_EDIT_DEMAND, {
+        method: 'POST',
+        body: JSON.stringify(editDemand),
+        headers: {
+          'content-type' : 'application/json'
+        }
+      });
     }
 
 }
